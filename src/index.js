8aed532a8fcfb51bf5060ce84e6d582b7d8c5f94
Cat <ccallcottstevens@gmail.com>
2020-07-02 18:28:58 +1000

    added an array, helper function and happy message component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const Greeting = () => {
-    return <h1> Hi there, my good friend!</h1>
+const Messages = [
+    'Well...the years start coming and they don\'t stop coming',
+    'Life is a highway and I\'m gonna ride it all night long',
+    'All I wanna do is have some fun'
+]
+
+const randomElement = Messages[Math.floor(Math.random() * Messages.length)];
+
+const HappyMessage = () => {
+    return <p>{ randomElement } </p>
 }
 
 const App = () => {
     return (
         <div>
-            <Greeting />
-            <Greeting />
+            <HappyMessage />
         </div>
     )
 }
